715eb727642f54a048e9135ac014ebe8cedf54f8
Anna Ibarra <annibrra@gmail.com>
2023-06-10 17:00:05 -0500

    Finish raw watchlist

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  resources :lists, only: %i[index show new create destroy] do
+    resources :bookmarks, only: %i[new create destroy]
+  end
+  resources :bookmarks, only: [:destroy]
 end
